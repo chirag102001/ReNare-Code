c8a66dc7dff28e8a40d599bfb205fde9b1b8fe1f
chirag102001 <123859491+chirag102001@users.noreply.github.com>
2023-12-05 20:45:29 +0100

    Add files via upload

---
+++ fhof/Prepostioning_values.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np
+from pathlib import Path
+
+# Define the working directory
+WORKDIR = Path(__file__).parent
+
+# Step 1: Import the CSV file containing velocity data
+velocity_df = pd.read_csv(WORKDIR / 'data_sensordata.csv', low_memory=False)
+
+# Step 2: Import the CSV file containing inertia data for multiple robot links
+inertial_df = pd.read_csv(WORKDIR / "parameters_inertial.csv", index_col="body_name", header=0)
+
+# Get the list of link IDs
+link_ids = inertial_df.index.to_list()
+
+# Create an empty DataFrame to store individual link energies
+link_energy_df = pd.DataFrame(index=range(1, 7), columns=['Total_Kinetic_Energy'])
+
+# Loop through the first six links
+for i in range(1, 7):
+    # Extract the required columns for linear and angular velocities for the current link
+    link_id = link_ids[i]  # Adjust the index to start from 0
+    linvel_columns = [f'{link_id}_linvel.{axis}' for axis in "xyz"]
+    angvel_columns = [f'{link_id}_angvel.{axis}' for axis in "xyz"]
+
+    linvel_df = velocity_df[linvel_columns]
+    angvel_df = velocity_df[angvel_columns]
+
+    # Get the mass and inertia data for the current link
+    link_mass = inertial_df.loc[link_id]["mass"]
+    link_inertial = np.diag(inertial_df.loc[link_id].loc[[f"inertia.{axis}" for axis in ("xx", "yy", "zz")]])
+
+    # Define a function to calculate kinetic energy for the current link
+    def calculate_kinetic_energy(row):
+        linvel_values = np.array(row[linvel_columns])
+        angvel_values = np.array(row[angvel_columns])
+
+        kinetic_energy = (
+            (link_mass * np.dot(linvel_values, linvel_values) / 2) +
+            (np.dot(angvel_values, np.dot(link_inertial, angvel_values)) / 2)
+        )
+
+        return kinetic_energy
+
+    # Filter rows based on the "step" column (replace 'prepositioning' with the actual value in your dataset)
+    prepositioning_rows = velocity_df[velocity_df['step'] == 'prepositioning']
+
+    # Apply the calculate_kinetic_energy function to each row and create a new column for the current link
+    prepositioning_rows[f'kinetic_energy_link_{i}'] = prepositioning_rows.apply(calculate_kinetic_energy, axis=1)
+
+    # Calculate the total kinetic energy for the current link by summing all row values
+    absolute_power = prepositioning_rows[f'kinetic_energy_link_{i}'].diff(1).abs()
+    total_kinetic_energy = absolute_power.sum() * prepositioning_rows["time"].mean()
+
+    # Store the total kinetic energy for the current link in the DataFrame
+    link_energy_df.loc[i, 'Total_Kinetic_Energy'] = total_kinetic_energy
+
+    # Display the total kinetic energy for the current link
+    print(f"Total Kinetic Energy (Link {i} - Prepositioning):", total_kinetic_energy)
+
+# Calculate and display the sum of kinetic energy across all links
+total_kinetic_energy_sum = link_energy_df['Total_Kinetic_Energy'].sum()
+print("Sum of Total Kinetic Energy across all links:", total_kinetic_energy_sum)
+    
---
+++ fhof/b_z_s code.py
@@ -0,0 +1,57 @@
+from pathlib import Path
+
+import pandas as pd
+import numpy as np
+
+WORKDIR = Path(__file__).parent
+
+# Step 1: Import the CSV file containing velocity data
+velocity_df = pd.read_csv(WORKDIR / 'data_sensordata.csv',low_memory=False)
+
+inertial_df = pd.read_csv(WORKDIR / "parameters_inertial.csv", index_col="body_name", header=0)
+link_ids = inertial_df.index.to_list()
+
+link_id = link_ids[1]  # should be robot_link_1
+# link_id_second = link_ids[2] # robot_link_2
+
+# Step 2: Extract the required columns for linear and angular velocities
+linvel_columns = [f'{link_id}_linvel.{axis}' for axis in "xyz"]
+angvel_columns = [f'{link_id}_angvel.{axis}' for axis in "xyz"]
+
+linvel_df = velocity_df[linvel_columns]
+angvel_df = velocity_df[angvel_columns]
+
+# # Step 3: Import the CSV file containing inertia data for multiple robot links
+# inertia_df = pd.read_csv('parameters_inertial.csv')
+
+# # Step 4: Filter the inertia data for robot link 1
+# link1_inertia_df = inertia_df[inertia_df['inertia.xx'] == 'robot_link_1']
+
+
+# # Step 5: Get the mass and inertia data for link 1
+# link1_mass = link1_inertia_df['mass'].values[0]
+# link1_inertial = np.array(link1_inertia_df[['inertia_xx', 'inertia_yy', 'inertia_zz']])
+
+link1_mass = inertial_df.loc[link_id]["mass"]
+link1_inertial = np.diag(inertial_df.loc[link_id].loc[[f"inertia.{axis}" for axis in ("xx", "yy", "zz")]])
+
+def calculate_kinetic_energy(row):
+    linvel_values = np.array(row[linvel_columns])
+    angvel_values = np.array(row[angvel_columns])
+
+    kinetic_energy = (
+        (link1_mass * np.dot(linvel_values, linvel_values) / 2) +
+        (np.dot(angvel_values, np.dot(link1_inertial, angvel_values)) / 2)
+    )
+
+    return kinetic_energy
+
+# Step 5: Apply the calculate_kinetic_energy function to each row and create a new column for the results
+velocity_df['kinetic_energy'] = velocity_df.apply(calculate_kinetic_energy, axis=1)
+
+# Step 6: Calculate the total kinetic energy by summing all row values
+absolute_power = velocity_df["kinetic_energy"].diff(1).abs()
+total_kinetic_energy = absolute_power.sum() * velocity_df["time"].mean()
+
+# Step 7: You can now access the total kinetic energy value
+print("Total Kinetic Energy:", total_kinetic_energy)
